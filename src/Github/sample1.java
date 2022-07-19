87c997ecfec65618a83a3f8bf509e4e13463f0cd
vjuma <vjuma@Vinayak>
2022-07-19 16:14:10 +0530

    [vinayak]:

---
+++ src/Github/sample1.java
@@ -0,0 +1,15 @@
+package Github;
+
+public class sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("adding new folder to the repository");
+		
+		System.out.println("hii");
+		
+		System.out.println("hello");
+		
+	}
+
+}
